1c5dd024a205891ef882f5cd33ecf47c4fdd095b
Julien Barrier <1346858+julienbarrier@users.noreply.github.com>
2021-08-16 14:16:51 +0100

    xdotool

--- .config/setup.sh
+++ .config/setup.sh
@@ -28,6 +28,7 @@ brew install wget
 brew tap zegervdv/zathura
 brew install zathura
 brew install zathura-pdf-poppler
+brew install xdotool
 mkdir -p $(brew --prefix zathura)/lib/zathura
 ln -s $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
 
